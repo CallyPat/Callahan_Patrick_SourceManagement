c13dfd4059926a7796d49ecad192aaab35906b43
CallyPat <patrick.callahan@sinclair.edu>
2021-10-23 17:33:53 -0400

    <First commit>Add project files.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+// Patrick Callahan
+// Cis 1202
+// 10/23/2021
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+
+	system("pause");
+	return 0;
+
+
+}
